96fcbd4cbc5484495d3b9578a018f9c6b7a2f976
John Hollander <john_hollander@hotmail.com>
2025-05-08 22:52:04 -0400

    Minor cleanup.

--- source/NetCoreServer.MAUI/FileCache.cs
+++ source/NetCoreServer.MAUI/FileCache.cs
@@ -93,7 +93,7 @@ namespace NetCoreServer
         /// <returns>'true' if the cache path was setup, 'false' if failed to setup the cache path</returns>
         public bool InsertPath(string path, string prefix = "/", string filter = "*.*", TimeSpan timeout = new TimeSpan(), InsertHandler handler = null)
         {
-            handler ??= (FileCache cache, string key, byte[] value, TimeSpan timespan) => cache.Add(key, value, timespan);
+            handler ??= (cache, key, value, timespan) => cache.Add(key, value, timespan);
 
             // Try to find and remove the previous path
             RemovePathInternal(path);
